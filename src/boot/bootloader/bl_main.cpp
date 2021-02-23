b1c8d845736e31eaa8ae11982a79a07c7e5069b5
Brandon Braun <brandonbraun653@gmail.com>
2021-02-22 22:43:11 -0600

    Add template for bootloader and mlo startup

---
+++ src/boot/bootloader/bl_main.cpp
@@ -0,0 +1,14 @@
+/********************************************************************************
+ *  File Name:
+ *    bl_main.cpp
+ *
+ *  Description:
+ *    Main entry point into the common system bootloader
+ *
+ *  2021 | Brandon Braun | brandonbraun653@gmail.com
+ *******************************************************************************/
+
+namespace Apollo::BootLoader
+{
+
+}  // namespace Apollo::BootLoader
---
+++ src/boot/mlo/boot_mlo.cpp
@@ -0,0 +1,14 @@
+/********************************************************************************
+ *  File Name:
+ *    boot_mlo.cpp
+ *
+ *  Description:
+ *    Powers on the processor and prepares the system to load the bootloader
+ *
+ *  2021 | Brandon Braun | brandonbraun653@gmail.com
+ *******************************************************************************/
+
+namespace Apollo::MLO
+{
+
+}  // namespace Apollo::MLO
