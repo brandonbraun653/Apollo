3242277e40ddab41715984f63f922307c859e126
Brandon Braun <brandonbraun653@gmail.com>
2021-02-27 21:21:16 -0600

    Add high level memory map for L3 & L4

---
+++ src/prcm/clk_mod_reg/reg_cm_per.hpp
@@ -0,0 +1,24 @@
+/********************************************************************************
+ *  File Name:
+ *    reg_cm_per.hpp
+ *
+ *  Description:
+ *    Register definitions for CM_PER_XXXX
+ *
+ *  2021 | Brandon Braun | brandonbraun653@gmail.com
+ *******************************************************************************/
+
+#pragma once
+#ifndef APOLLO_REG_DEF_CM_PER_HPP
+#define APOLLO_REG_DEF_CM_PER_HPP
+
+/* Chimera Includes */
+#include <Chimera/common>
+#include <Chimera/utility>
+
+namespace Apollo::Reg
+{
+
+}  // namespace Apollo::Reg
+
+#endif  /* !APOLLO_REG_DEF_CM_PER_HPP */
